1d9281a046328e72c71b2cd7626d94355eb16403
primado <primadoprincepa@gmail.com>
2024-03-23 03:17:28 +0000

    neglieable Pycharm PEP8 changes

--- recipe/models.py
+++ recipe/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import CustomUser
 
+
 # Create your models here.
 
 class Recipe(models.Model):
@@ -9,7 +10,7 @@ class Recipe(models.Model):
     ingredient = models.TextField(blank=True)
     instruction = models.TextField(blank=True)
     cooking_time = models.IntegerField(blank=True)
-    
+
     DIFFICULTY_LEVEL_CHOICES = [
         ("easy", "Easy"),
         ("medium", "Medium"),
@@ -20,10 +21,10 @@ class Recipe(models.Model):
     VISIBILITY_CHOICES = [
         (PUBLIC, "Public"),
         (PRIVATE, "Private")
-    ]    
+    ]
 
     visibility = models.CharField(max_length=7, choices=VISIBILITY_CHOICES)
-    difficulty_level = models.CharField(max_length=7,choices=DIFFICULTY_LEVEL_CHOICES)
+    difficulty_level = models.CharField(max_length=7, choices=DIFFICULTY_LEVEL_CHOICES)
     recipe_image = models.ImageField(upload_to='recipe-images', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateField(auto_now=True)
@@ -31,7 +32,7 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         db_table = 'Recipe'
         managed = True
@@ -60,8 +61,9 @@ class RecipeCollection(models.Model):
 
 
 class RecipeCollectionRecipe(models.Model):
-    recipe = models.ForeignKey(Recipe,  on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     collection = models.ForeignKey(RecipeCollection, on_delete=models.CASCADE)
+
 
 class Comment(models.Model):
     text = models.TextField()
@@ -71,13 +73,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-    
+
     class Meta:
         db_table = 'Comment'
         managed = True
         verbose_name = 'comment'
         verbose_name_plural = 'comments'
         ordering = ['-created_at']
+
 
 class Rating(models.Model):
     UPVOTE = 'upvote'
@@ -98,13 +101,12 @@ class Tag(models.Model):
     name = models.CharField(max_length=30)
     created_at = models.DateField(auto_now_add=True)
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
-    
+
     class Meta:
         db_table = 'Tag'
         managed = True,
         verbose_name = "tag"
         verbose_name_plural = "tag"
         ordering = ['-created_at']
-
